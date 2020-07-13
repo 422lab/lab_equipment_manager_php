0d738406c30e563954cf680163562b88f808a79b
Jack Chen <redchenjs@live.com>
2020-07-13 14:25:50 +0800

    index: update comments

--- index.php
+++ index.php
@@ -13,39 +13,39 @@
 
 $data = file_get_contents("php://input");   // 获取POST数据
 $data = json_decode($data, true);           // 解析JSON
-$request = $data['request'];                // 收到$request请求代码;Number;3位数字;
+$request = $data['request'];                // 接收$request请求号;Number;3位数字;
 
 switch ($request) {
 case 100:   // 设备端同步请求
-    $status = $data['status'];              // 收到$status设备状态;String;"on":开启,"off":关闭;
-    $mac_addr = $data['mac'];               // 收到$mac设备MAC地址;String;"30:ae:a4:56:75:50";
+    $status = $data['status'];              // 接收$status设备状态;String;"on":已上机,"off":未上机;
+    $mac_addr = $data['mac'];               // 接收$mac设备MAC地址;String;"30:ae:a4:56:75:50";
     $arr = array(
-        'request' => 100,                   // 发送$request请求号;Number;将发来的$request原样送回;
-        'status'  => true,                  // 发送$status请求结果;Bool;未上机时,true:设备已注册,false:设备未注册;已上机时,true:保持上机,false:强制下机;
-        'token'   => md5(time()),           // 发送$token设备令牌;String;32位MD5,用于二维码编码,每分钟更新一次,未注册设备不发送;
-        'user_info'  => "Xb14610214",       // 发送$user_info用户信息;String;10位字符串,未上机时不发送;
-        'timer_time' => "09:00:00"          // 发送$timer_time预约时间;String;"HH:MM:SS",24小时制,未上机时不发送;
+        'request' => 100,                   // 发送$request请求号;Number;将接收到的请求号原样送回;
+        'status'  => true,                  // 发送$status请求结果;Bool;未上机时,true:更新二维码数据,false:保持二维码数据;已上机时,true:保持上机,false:强制下机;
+        'token'   => md5(time()),           // 发送$token设备令牌;String;32位MD5,二维码数据,未上机且有更新时发送;
+        'user_info'  => "Xb14610214",       // 发送$user_info用户信息;String;10位字符串(学工号),已上机且有更新时发送;
+        'timer_time' => "15:00:00"          // 发送$timer_time预约时间;String;"HH:MM:SS",24小时制,已上机且有更新时发送;
     );
     header('content-type:application/json');
     echo json_encode($arr);
     break;
 case 101:   // 设备端下机通知
-    $status = $data['status'];              // 收到$status设备状态;String;"on":开启,"off":关闭;
-    $mac_addr = $data['mac'];               // 收到$mac设备MAC地址;String;"30:ae:a4:56:75:50";
+    $status = $data['status'];              // 接收$status设备状态;String;"on":已上机,"off":未上机;
+    $mac_addr = $data['mac'];               // 接收$mac设备MAC地址;String;"30:ae:a4:56:75:50";
     $arr = array(
-        'request' => 101                    // 发送$request请求号;Number;将发来的$request原样送回;
+        'request' => 101                    // 发送$request请求号;Number;将接收到的请求号原样送回;
     );
     header('content-type:application/json');
     echo json_encode($arr);
     break;
 case 102:   // 设备端上机请求
-    $status = $data['status'];              // 收到$status设备状态;String;"on":开启,"off":关闭;
-    $mac_addr = $data['mac'];               // 收到$mac设备MAC地址;String;"30:ae:a4:56:75:50";
+    $status = $data['status'];              // 接收$status设备状态;String;"on":已上机,"off":未上机;
+    $mac_addr = $data['mac'];               // 接收$mac设备MAC地址;String;"30:ae:a4:56:75:50";
     $arr = array(
-        'request' => 102,                   // 发送$request请求号;Number;将发来的$request原样送回;
+        'request' => 102,                   // 发送$request请求号;Number;将接收到的请求号原样送回;
         'status'  => true,                  // 发送$status请求结果;Bool;true:允许上机,false:不允许上机;
-        'user_info'  => "Xb14610214",       // 发送$user_info用户信息;String;10位字符串,不允许上机时不发送;
-        'timer_time' => "09:00:00"          // 发送$timer_time预约时间;String;"HH:MM:SS",24小时制,不允许上机时不发送;
+        'user_info'  => "Xb14610214",       // 发送$user_info用户信息;String;10位字符串(学工号),允许上机时发送;
+        'timer_time' => "15:00:00"          // 发送$timer_time预约时间;String;"HH:MM:SS",24小时制,允许上机时发送;
     );
     header('content-type:application/json');
     echo json_encode($arr);
