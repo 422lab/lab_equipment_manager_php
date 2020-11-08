8a60fd7cdbcb806e4fefe3b90c9f69885bdeaf6d
Jack Chen <redchenjs@live.com>
2020-11-08 18:14:10 +0800

    index: code formatting

--- index.php
+++ index.php
@@ -7,49 +7,49 @@
  * @package  None
  * @author   Jack Chen <redchenjs@live.com>
  * @license  https://server.zyiot.top/lem public
- * @version  GIT: <v0.1-draft>
+ * @version  GIT: <v0.2-draft>
  * @link     https://server.zyiot.top/lem
  */
 
-const HTTP_REQ_CODE_UPD = 100;  // 设备端同步请求
-const HTTP_REQ_CODE_OFF = 101;  // 设备端下机通知
-const HTTP_REQ_CODE_ON  = 102;  // 设备端上机请求
+const HTTP_REQ_CODE_DEV_UPD = 100;  // 设备端请求获取更新
+const HTTP_REQ_CODE_DEV_OFF = 101;  // 设备端发送下机通知
+const HTTP_REQ_CODE_DEV_ON  = 102;  // 设备端请求允许上机
 
 $data = file_get_contents("php://input");   // 获取POST数据
 $data = json_decode($data, true);           // 解析JSON
-$code = $data['code'];                      // 接收$code请求号;Number;3位数字;
+$code = $data['request'];                   // 客户端请求码
 
 switch ($code) {
-case HTTP_REQ_CODE_UPD:
-    $status   = $data['status'];            // 接收$status设备状态;String;"on":已上机,"off":未上机;
-    $wifi_mac = $data['wifi_mac'];          // 接收$wifi_mac设备MAC地址;String;"30:ae:a4:56:75:50";
+case HTTP_REQ_CODE_DEV_UPD:
+    $device_mac   = $data['device_mac'];    // 获取$device_mac设备MAC地址;String;"30:ae:a4:56:75:50";
+    $relay_status = $data['relay_status'];  // 获取$relay_status设备继电器状态;String;"on":已开启,"off":未开启;
     $arr = array(
-        'code'        => $code,             // 发送$code请求号;Number;将接收到的请求号原样送回;
-        'status'      => true,              // 发送$status请求结果;Bool;未上机时,true:更新二维码数据,false:保持二维码数据;已上机时,true:保持上机,false:强制下机;
-        'token'       => md5(time()),       // 发送$token设备令牌;String;32位MD5,二维码数据,未上机且有更新时发送;
-        'user_info'   => 'Xb14610214',      // 发送$user_info用户信息;String;10位字符串(学工号),已上机且有更新时发送;
-        'expire_time' => '20:00:00'         // 发送$expire_time预约到期时间;String;"HH:MM:SS",24小时制,已上机且有更新时发送;
+        'code'        => $code,             // 返回$code请求号;Number;将接收到的请求号原样送回;
+        'status'      => true,              // 返回$status请求结果;Bool;未上机时,true:更新二维码数据,false:保持二维码数据;已上机时,true:保持上机,false:强制下机;
+        'qrcode'      => md5(time()),       // 返回$qrcode二维码数据;String;32位MD5字符串,未上机且有更新时发送;
+        'user_info'   => 'Xb14610214',      // 返回$user_info用户信息;String;10位字符串(学工号),已上机且有更新时发送;
+        'expire_time' => '20:00:00'         // 返回$expire_time预约到期时间;String;"HH:MM:SS",24小时制,已上机且有更新时发送;
     );
     header('content-type:application/json');
     echo json_encode($arr);
     break;
-case HTTP_REQ_CODE_OFF:
-    $status   = $data['status'];            // 接收$status设备状态;String;"on":已上机,"off":未上机;
-    $wifi_mac = $data['wifi_mac'];          // 接收$wifi_mac设备MAC地址;String;"30:ae:a4:56:75:50";
+case HTTP_REQ_CODE_DEV_OFF:
+    $device_mac   = $data['device_mac'];    // 获取$device_mac设备MAC地址;String;"30:ae:a4:56:75:50";
+    $relay_status = $data['relay_status'];  // 获取$relay_status设备继电器状态;String;"on":已开启,"off":未开启;
     $arr = array(
-        'code' => $code                     // 发送$code请求号;Number;将接收到的请求号原样送回;
+        'code' => $code                     // 返回$code请求号;Number;将接收到的请求号原样送回;
     );
     header('content-type:application/json');
     echo json_encode($arr);
     break;
-case HTTP_REQ_CODE_ON:
-    $status   = $data['status'];            // 接收$status设备状态;String;"on":已上机,"off":未上机;
-    $wifi_mac = $data['wifi_mac'];          // 接收$wifi_mac设备MAC地址;String;"30:ae:a4:56:75:50";
+case HTTP_REQ_CODE_DEV_ON:
+    $device_mac   = $data['device_mac'];    // 获取$device_mac设备MAC地址;String;"30:ae:a4:56:75:50";
+    $relay_status = $data['relay_status'];  // 获取$relay_status设备继电器状态;String;"on":已开启,"off":未开启;
     $arr = array(
-        'code'        => $code,             // 发送$code请求号;Number;将接收到的请求号原样送回;
-        'status'      => true,              // 发送$status请求结果;Bool;true:允许上机,false:不允许上机;
-        'user_info'   => 'Xb14610214',      // 发送$user_info用户信息;String;10位字符串(学工号),允许上机时发送;
-        'expire_time' => '20:00:00'         // 发送$expire_time预约到期时间;String;"HH:MM:SS",24小时制,允许上机时发送;
+        'code'        => $code,             // 返回$code请求号;Number;将接收到的请求号原样送回;
+        'status'      => true,              // 返回$status请求结果;Bool;true:允许上机,false:不允许上机;
+        'user_info'   => 'Xb14610214',      // 返回$user_info用户信息;String;10位字符串(学工号),允许上机时发送;
+        'expire_time' => '20:00:00'         // 返回$expire_time预约到期时间;String;"HH:MM:SS",24小时制,允许上机时发送;
     );
     header('content-type:application/json');
     echo json_encode($arr);
